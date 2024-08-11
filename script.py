ab3dba3523da80adf95742df57074b9477bdf342
Zain Ul Aabedin <zainulaabedintts@gmail.com>
2024-08-11 12:49:33 +0000

    Python Variable,Arthimetic operator

---
+++ script.py
@@ -0,0 +1,42 @@
+print("Hello World!")
+print(127)
+name = "zain Ul Aabedin"
+
+# Variables
+
+print("Name", name)
+age = 20
+print ("My age is =", age)
+
+# Operators
+#  -Arthimetic Operators
+# +,-,/,*,**,//,%
+
+num1 = 20
+num2 = 24
+
+addition= num1 + num2
+print("Addition", addition)
+
+subtraction = num2 -num1
+print("Subtraction", subtraction)
+
+division = 434/2
+print("Division =", division)
+
+multiplication = 31* 4
+print("Multiplication =", multiplication)
+
+
+test =10/2*5+9-2+3 #DMAS
+print("DMAS Rule =",test)
+
+
+exp =2**10
+print("Exponent =",exp)
+
+div = 10//2
+print("Division for integer =", div)
+
+reminder =10%3
+print("Modulus/Reminder =", reminder)
